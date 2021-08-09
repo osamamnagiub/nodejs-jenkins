71904065ccbac18424074a8a79bc12984added82
osama <osama.mn@live.com>
2021-08-09 15:12:56 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+});
+
+
+const server = app.listen(3000, (a) => {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log(`listening on http://${host}:${port}`);
+});
+
